ae0538061cb30b647a6dd068a2a59959f2d6a01a
Aryan Khunt <151055762+khuntAryan@users.noreply.github.com>
2024-12-18 22:08:57 +0530

    Completing the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { ThemeProvider } from './context/theme'
+import useTheme from './context/theme'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ themeMode , setThemeMode ] = useState("lightMode")
+
+  const lightMode = () => {
+    setThemeMode("lightMode")
+  }
+  const darkMode = () => {
+    setThemeMode("darkMode")
+  }
+
+  useEffect(()=>{
+    document.querySelector('html').classList.remove('lightMode','darkMode')
+    document.querySelector('html').classList.add(themeMode)
+  },[themeMode])
 
   return (
-    <>
-      <h1>Project based on Context</h1>
-    </>
+    <ThemeProvider value={{themeMode , lightMode , darkMode}}>
+    <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  <ThemeBtn />
+              </div>
+
+              <div className="w-full max-w-sm mx-auto">
+                  <Card />
+              </div>
+          </div>
+      </div>
+      </ThemeProvider>
   )
 }
 
